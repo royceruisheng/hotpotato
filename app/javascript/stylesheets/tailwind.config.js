727835ce7eebcbf4c320062d4c699562424a0e29
royceruisheng <ruisheng.lim@gmail.com>
2021-09-21 14:42:41 +0800

    rails new

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,17 @@
+/* app/javascript/stylesheets/tailwind.config.js */
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
