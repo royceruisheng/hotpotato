9b7cad198d4466b9335b34be4ea2837065d26567
royceruisheng <ruisheng.lim@gmail.com>
2021-09-21 15:55:31 +0800

    fixed config

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,10 +1,10 @@
 /* app/javascript/stylesheets/tailwind.config.js */
 module.exports = {
   purge: [
-    "./app/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/javascript/**/*.vue",
+    // "./app/**/*.html.erb",
+    // // "./app/helpers/**/*.rb",
+    // "./app/javascript/**/*.js",
+    // "./app/javascript/**/*.vue",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
