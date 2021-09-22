0ad665e2c6ea4c040f28085c0c7cbebac3a2583a
royceruisheng <ruisheng.lim@gmail.com>
2021-09-22 12:28:09 +0800

    revert cloudinary settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :photo
+  # has_one_attached :photo
 
 end
