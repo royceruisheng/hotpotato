04b5d535a9a15f80f70513e142df44a539c3ced9
ethan pay <ethancy.pay@gmail.com>
2021-09-23 11:09:00 +0800

    rebase

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,15 +1,4 @@
 class WorkflowsController < ApplicationController
-  before_action :set_workflow, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @workflow = Workflow.all # singular??
-  end
-
-  def new
-    @workflow = Workflow.new
-    # is activated false set here?
-    # is template even set?
-  end
 
   def create
     new_workflow = Workflow.new(users_id: current_user.id, title: 'New Workflow')
@@ -17,22 +6,4 @@ class WorkflowsController < ApplicationController
     redirect_to dashboard_path
   end
 
-  def update
-    @workflow.update(params[:workflow])
-  end
-
-  def destroy
-    @wirkflow.destroy
-    redirect_to dashboard_path
-  end
-
-  private
-
-  def set_workflow
-    @workflow = Workflow.find(params[:id])
-  end
-
-  # def workflow_params
-  #   params.require(:workflow).permit(:title, :description, :users_id)
-  # end
 end
