0af92c3fe46c835e504c144834b080a58c9b34bd
royceruisheng <ruisheng.lim@gmail.com>
2021-09-23 09:38:22 +0800

    quick fix to model and disabled cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  has_one_attached :photo
+  # has_one_attached :photo
 
-  has_and_belongs_to_many :workflows#, class_name: "Workflows", foreign_key: "creator_id"
+  has_many :workflows, class_name: "Workflows", foreign_key: "creator_id"
   has_many :task_members, dependent: :destroy
   has_many :tasks, through: :task_members
   has_many :item_members, dependent: :destroy
--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -1,7 +1,7 @@
 class Workflow < ApplicationRecord
   validates :title, presence: true
 
-  has_and_belongs_to_many :users
+  belongs_to :users
   has_many :tasks, dependent: :destroy
 
 end
