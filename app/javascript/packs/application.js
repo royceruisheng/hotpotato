7da328a1c7123d461088eda3a846e34c8ce03faa
royceruisheng <ruisheng.lim@gmail.com>
2021-09-24 16:40:23 +0800

    added trix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 import "stylesheets/application" // Add this line
 
