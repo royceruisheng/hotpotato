e2de2c94a2ee5cbb54865d046842313cd6991ee1
Daniel Teo <teodaniel14@gmail.com>
2021-09-24 21:17:43 +0800

    add member action transfer after rebase; to work on: make member display properly when added

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -2,7 +2,7 @@ import { csrfToken } from "@rails/ujs";
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["new", "newform", "form", "list", 'taskname']
+  static targets = ["new", "newform", "form", "list", 'taskname', "member", "membernames" ]
 
   connect() {
     console.log("task controller connected")
@@ -47,5 +47,25 @@ export default class extends Controller {
     this.listTarget.insertAdjacentHTML('beforeend', newTask)
     this.formTarget.classList.toggle("hidden")
     this.newTarget.classList.toggle("hidden")
+  }
+
+  // action to add a member in a task (requires task_id)
+  add(e) {
+    // console.log(this.memberTarget);
+    e.preventDefault();
+    const url = `/tasks/${ this.element.dataset.taskId }/task_members/`
+    // how to pass in the params of user??
+    fetch(url, { 
+        method: 'POST', 
+        headers: { 'Accept': 'text/plain', 'X-CSRF-token': csrfToken() }, 
+        body: JSON.stringify({ task_member_id: this.membernamesTarget.dataset.memberId })
+    })
+    .then(response => response.text())
+    .then(this.addMember.bind(this))
+  }
+
+  addMember(member) {
+    console.log(member)
+    this.memberTarget.insertAdjacentHTML( 'afterbegin', member.first_name )
   }
 }
--- app/javascript/controllers/task_member_controller.js
+++ app/javascript/controllers/task_member_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ "dropdown", "member", "membernames" ]
+  static targets = [ "dropdown" ]
 
   connect() {
     console.log("task member controller connected");
@@ -23,34 +23,24 @@ export default class extends Controller {
   }
 
 
-  add(e) {
-    // console.log(this.memberTarget);
-    e.preventDefault();
-    const url = `/tasks/${ this.element.dataset.taskId }/task_members/`
-    // how to pass in the params of user??
-    fetch(url, { 
-        method: 'POST', 
-        headers: { 'Accept': 'text/plain', 'X-CSRF-token': csrfToken() }, 
-        body: JSON.stringify({ task_member_id: this.membernamesTarget.dataset.memberId })
-    })
-    .then(response => response.text())
-    .then(this.addMember.bind(this))
-  }
+  // function transferred to task_controller.js for bugfixing
 
-  addMember(member) {
-    console.log(member)
-    this.memberTarget.insertAdjacentHTML( 'afterbegin', member.first_name )
-  }
-
-  // submitForm(e){
-  //   e.preventDefault()
-  //   Rails.fire(this.formTarget, 'submit')
+  // add(e) {
+  //   // console.log(this.memberTarget);
+  //   e.preventDefault();
+  //   const url = `/tasks/${ this.element.dataset.taskId }/task_members/`
+  //   // how to pass in the params of user??
+  //   fetch(url, { 
+  //       method: 'POST', 
+  //       headers: { 'Accept': 'text/plain', 'X-CSRF-token': csrfToken() }, 
+  //       body: JSON.stringify({ task_member_id: this.membernamesTarget.dataset.memberId })
+  //   })
+  //   .then(response => response.text())
+  //   .then(this.addMember.bind(this))
   // }
 
-
-  // sampleFunction() {
-  //   fetch(whatever)
-  //     .then(res => res.json())
-  //     .then(data => this.doSomething(data).bind(this))
+  // addMember(member) {
+  //   console.log(member)
+  //   this.memberTarget.insertAdjacentHTML( 'afterbegin', member.first_name )
   // }
 }
