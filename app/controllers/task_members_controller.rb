f87b265ce542a8b77ce4c42f26e58df5bbfc0b49
Daniel Teo <teodaniel14@gmail.com>
2021-09-24 11:11:02 +0800

    commit for rebase

--- app/controllers/task_members_controller.rb
+++ app/controllers/task_members_controller.rb
@@ -3,13 +3,22 @@ class TaskMembersController < ApplicationController
     @task = Task.find(params[:task_id])
     @user = User.find(params[:user_id])
     @task_member = TaskMember.new
-    
-    redirect_to 
+
     if @task_member.save
       redirect_to dashboard_path(@user, @task)
     else
       render 'dashboard/index'
     end
+
+    # respond_to do |format|
+    #   if @task_member.save
+    #     format.html { redirect_to dashboard_path(@task_member) }
+    #     format.json # Follow the classic Rails flow and look for a create.json view
+    #   else
+    #     format.html { render 'dashboards' }
+    #     format.json # Follow the classic Rails flow and look for a create.json view
+    #   end
+    # end
   end
 
   private
--- app/javascript/controllers/member_controller.js
+++ app/javascript/controllers/member_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus"
+import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
   static targets = [ "dropdown" ]
@@ -8,7 +9,17 @@ export default class extends Controller {
     this.dropdownTarget.classList.toggle("hidden")
   }
 
-  add() {
-    // console.log("button is connected");
+  add(event) {
+    event.preventDefault();
+
+    fetch(this.dropdownTarget.action, {
+      method: 'POST',
+      headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken() },
+      body: new DropdownData(this.dropdownTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      });
   }
 }
