0f88c266989bab9c2b15ddaec1c4ddf7a186cf5c
ethan pay <ethancy.pay@gmail.com>
2021-09-26 12:15:21 +0800

    workflows can now be activated

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -6,7 +6,7 @@ class WorkflowsController < ApplicationController
     title_check = Workflow.where(title: 'New Workflow')
     title = title_check[0] ? "#{title_check[0].title} copy" : 'New Workflow'
 
-    new_workflow = Workflow.new(title: title, activated: 'inactive')
+    new_workflow = Workflow.new(title: title)
     new_workflow.creator = current_user
     new_workflow.save
 
@@ -30,18 +30,25 @@ class WorkflowsController < ApplicationController
     set_workflows_and_task
 
     @workflow = Workflow.find(params[:id])
-    @workflow.update(workflow_params)
+    @workflow.update(workflow_params) # updates title
 
     render 'dashboard/index'
   end
 
   def activate
+    set_workflows_and_task
+
     workflow_params = JSON.parse(request.body.read)
-    @workflow = Workflow.find(workflow_params[:workflowId])
-    @workflow.update(activated: 'active')
+    @workflow = Workflow.find(workflow_params['workflowId'])
+    if @workflow.activated
+      @workflow.update(activated: false)
+    else
+      @workflow.update(activated: true)
+    end
 
     respond_to do |format|
-      format.text { render partial: 'workflows/workflow_content', locals: { workflow: @workflow }, formats: [:html] }
+      format.text { render 'dashboard/index', formats: [:html] }
+      # format.text { render partial: 'workflows/workflow_content', locals: { workflow: @workflow }, formats: [:html] }
     end
   end
 
