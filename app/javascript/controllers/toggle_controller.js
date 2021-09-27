6e209a0b3d82267383e58b929f26662ddc1d1923
ethan pay <ethancy.pay@gmail.com>
2021-09-28 07:52:07 +0800

    solved addmember dropdown running when multiple tasks open

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,14 +2,13 @@ import { Controller } from "stimulus"
 import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ "dropdown", "hide" ]
+  static targets = [ "membersdropdown", "hide" ]
 
   connect() {
     console.log("toggle controller connected");
   }
   //  dropdown for the add members function
   dropdownAddMembers() {
-    
     const url = "/task_members/new"
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
@@ -17,15 +16,14 @@ export default class extends Controller {
   }
 
   updateAndHide(members) {
-    this.dropdownTarget.classList.toggle("hidden");
-    this.dropdownTarget.innerHTML = ""
-    this.dropdownTarget.insertAdjacentHTML('afterbegin', members )
+    this.membersdropdownTarget.classList.toggle("hidden");
+    this.membersdropdownTarget.innerHTML = ""
+    this.membersdropdownTarget.insertAdjacentHTML('afterbegin', members )
   }
 
+  // generic hider
   hide() {
     this.hideTarget.classList.toggle('hidden')
-
   }
-
 
 }
