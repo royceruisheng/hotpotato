52d603a3b15bd9998d8678127b323ab38ad8f695
royceruisheng <ruisheng.lim@gmail.com>
2021-09-27 18:56:18 +0800

    fixed path in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: root_path }
+  config.action_mailer.default_url_options = { host: "http://www.taskete.co" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
