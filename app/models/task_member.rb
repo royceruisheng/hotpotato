f2a24bfc6a923ffaf26c2d97694d6e3927129f0b
royceruisheng <ruisheng.lim@gmail.com>
2021-09-29 17:19:17 +0800

    fixing recipient pointing

--- app/models/task_member.rb
+++ app/models/task_member.rb
@@ -1,8 +1,4 @@
 class TaskMember < ApplicationRecord
   belongs_to :task
   belongs_to :user
-
-  def notifications
-    UserMailer.with(user: user, task: task).email_notification.deliver_now
-  end
 end
