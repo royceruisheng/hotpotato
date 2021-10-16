3492fbacca33b69e8556a64efb1916c0d48d466d
ethan pay <ethancy.pay@gmail.com>
2021-10-16 16:31:16 +0800

    improved sign up, disabled new user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :send_welcome_email
+  # after_create :send_welcome_email # causing error
 
   # validates :email, presence: true
   # validates :first_name, presence: true
